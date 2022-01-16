bbc0682c9420d45824e5d6708b36decb44c4f990
Rostyslaw Kostyk <rkostyk64@gmail.com>
2022-01-17 00:19:55 +0200

    base

--- README.old.md
+++ README.old.md
@@ -1,1 +1,3 @@
-# goit-react-hw-06-phonebook
+# goit-react-hw-05-movies
+
+My React homework #05. API key is c5d38a7be9bd3b66714f62495d2f9344.
--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
-import { BrowserRouter } from "react-router-dom";
-import MainMenu from './client/MainMenu';
+import { lazy, Suspense } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import Navigation from './components/Navigation';
+
 import './App.css';
-import Routes from "./routes";
+
+const HomeView = lazy(() =>
+  import('./views/HomeView/HomeView.js' /* webpackChunkName: "home-view"*/),
+);
+const MoviesView = lazy(() =>
+  import('./views/MoviesView/MoviesView.js' /* webpackChunkName: "movies-view"*/),
+);
+const NotFoundView = lazy(() =>
+  import('./views/NotFoundView/NotFoundView.js' /* webpackChunkName: "not-found-view"*/),
+);
+const MovieDetailsView = lazy(() =>
+  import(
+    './views/MovieDetailsView/MovieDetailsView.js' /* webpackChunkName: "movie-details-view"*/
+  ),
+);
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <MainMenu />
-        <Routes />
-      </div>
-    </BrowserRouter>
+    <div>
+      <Navigation />
+      <Suspense fallback={<p>...loading. Please, wait!</p>}>
+        <Switch>
+          <Route path="/" exact>
+            <HomeView />
+          </Route>
+          <Route exact path="/movies">
+            <MoviesView />
+          </Route>
+          <Route path="/movies/:movieId">
+            <MovieDetailsView />
+          </Route>
+          <Route>
+            {/* <NotFoundView /> */}
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Suspense>
+    </div>
   );
 }
 
